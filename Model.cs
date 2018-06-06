da9393b64cb94f2e400f54d7d4aa440b5ac13cbb
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2018-06-06 22:40:24 +0200

    Working sample code

---
+++ Model.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+/*
+ * Anlegen:
+ * dotnet ef migrations add InitialCreate
+ * dotnet ef database update
+ *
+ * Inhalt anschauen: mit sqlitebrowser
+ * apt install sqlitebrowser
+ */
+namespace ManyToMany
+{
+    public class ManyToManyContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Dozent> Dozents { get; set; }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=manytomany.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bekanntschaft>()
+                .HasKey(t => new { t.DozentId, t.StudentId });
+
+            modelBuilder.Entity<Bekanntschaft>()
+                .HasOne(pt => pt.Student)
+                .WithMany(p => p.Bekanntschaften)
+                .HasForeignKey(pt => pt.StudentId);
+
+            modelBuilder.Entity<Bekanntschaft>()
+                .HasOne(pt => pt.Dozent)
+                .WithMany(t => t.Bekanntschaften)
+                .HasForeignKey(pt => pt.DozentId);
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Matrikelnummer { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        public List<Bekanntschaft> Bekanntschaften { get; set; }
+    }
+
+    public class Dozent
+    {
+        public int DozentId { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        public List<Bekanntschaft> Bekanntschaften { get; set; }
+    }
+
+    public class Bekanntschaft 
+    {        
+        public Dozent Dozent { get; set; }
+        public int DozentId { get; set; }
+     
+        public Student Student { get; set; }
+        public int StudentId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManyToMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beispielanwendung für Many-to-Many mit Dotnet Core und EF-Core");
+
+            using(var ctx = new ManyToManyContext()) {
+                // Falls nötig, Teststudenten anlegen
+                for(int i = 0; i < 100; i++) {
+                    if(ctx.Students.Where(s => s.StudentId == i).Count() == 0) {
+                        var s = new Student() {
+                            StudentId = i,
+                            Matrikelnummer = $"12345678{i}",
+                            Vorname = "Max",
+                            Nachname = "Mustermann"
+                        };
+                        ctx.Students.Add(s);
+                        ctx.SaveChanges();
+                    }
+                }
+
+                // Falls nötig, Testdozent anlegen
+                if(ctx.Dozents.Where(d => d.DozentId == 1).Count() == 0) {
+                    var d = new Dozent() {
+                        DozentId = 1,
+                        Vorname = "Moritz",
+                        Nachname = "Musterlehrer"
+                    };
+                    ctx.Dozents.Add(d);
+                    ctx.SaveChanges();
+                }
+
+                var st = ctx.Students.Where(s => s.StudentId == 1).First();
+                var dz = ctx.Dozents.Where(d => d.DozentId == 1).First();
+
+                // Beziehung hinzufügen
+                var bez = new Bekanntschaft() {
+                    Student = st,
+                    Dozent = dz
+                };
+
+                if(st.Bekanntschaften == null) {
+                    st.Bekanntschaften = new List<Bekanntschaft>();
+                }
+
+                if(dz.Bekanntschaften == null) {
+                    dz.Bekanntschaften = new List<Bekanntschaft>();
+                } 
+
+                st.Bekanntschaften.Add(bez);
+                dz.Bekanntschaften.Add(bez);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
